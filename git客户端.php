4dc3aafadaeced4303f5b02dd8250a42388986a6
mojianhua <1657210793@qq.com>
2020-02-08 16:33:27 +0800

    git服务器搭建和客户端命令

---
+++ git客户端.php
@@ -0,0 +1,26 @@
+<?php
+/*
+1、查看git状态
+git status
+2、添加文件到版本控制
+git add 文件名，如果需要添加所有文件，则：git add .(英文句话)
+3、提交
+git commit -m ’注释’
+4、git 初始化
+git init
+5、关联远程(origin)仓库
+git remote add origin [git路径]
+6、推送到远程仓库
+git push -u origin master
+7、版本回退
+git reset —hard [版本号]
+9、查看分支
+git branch
+10、创建分支
+git branch [分支名]
+11、切换分支
+git checkout [分支名]
+12、新建并且创建分支
+git checkout -b [分支名]
+13、合并分支
+git merge [分支名]
---
+++ git服务端搭建.php
@@ -0,0 +1,29 @@
+<?php
+/*
+1、添加git用户
+sudo adduser jim_git
+2、添加用户名为git的密码
+sudo passwd jim_git
+3、创建用户组名为git的用户组
+sudo groupadd jim_git
+4、将用户名为git的用户添加到git用户组
+sudo usermod -G jim_git jim_git
+5、选择目录（略）
+6、创建git目录
+mkdir test_docs.git
+7、进入目录(略)
+8、初始化仓库—bare裸仓库
+git init —bare
+9、修改仓库权限为git用户
+sudo chown -R jim_git:jim_git /srv/test_docs.git
+10、禁止git用户通过shell登录
+	10.1:设置git的git shell
+		chsh git
+	10.2:路径
+		/usr/bin/git-shell
+11、免密拉去
+	11.1：生成ssh
+	ssh-keygen -t rsa
+	11.2：生成authorized_keys
+	touch 700 authorized_keys
+	13.3：将自己的id_rsa.pub存入authorized_keys，一个key一行
